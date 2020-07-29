70df4b5a6b98e8386bf11715b4626972032406c5
spacexjedi <58277328+spacexjedi@users.noreply.github.com>
2020-07-29 13:01:24 -0300

    Update comands.sh

--- comands.sh
+++ comands.sh
@@ -3,3 +3,6 @@ git clone -b stable https://github.com/flutter/flutter.git
 flutter doctor
 
 na barra do vscode digita flutter e cria o nome do projeto
+
+# dibrando o firebase
+flutter create --org br.com.spacex nomedoprojeto
