b384b98877942cc7ad94df10f391e050fc37e2f9
spacexjedi <58277328+spacexjedi@users.noreply.github.com>
2020-07-29 12:57:55 -0300

    Update comands.sh

--- comands.sh
+++ comands.sh
@@ -1,1 +1,5 @@
-git clone -b stable https://github.com/flutter/flutter.git
+git clone -b stable https://github.com/flutter/flutter.git  
+
+flutter doctor
+
+na barra do vscode digita flutter e cria o nome do projeto
